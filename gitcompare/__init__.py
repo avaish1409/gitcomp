b6228640da2f864b7ee010b502249a08be9200f1
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-05-01 21:55:08 +0530

    add main(), refactor arg parser and logger setups into methods

--- gitcompare/__init__.py
+++ gitcompare/__init__.py
@@ -10,6 +10,8 @@ from prettytable import PrettyTable, ALL
 import logging
 
 
+# TODO add function descriptions
+
 # get user or repo data
 def user_or_repo(arr, option='user'):
     # base url
@@ -61,86 +63,87 @@ def create_csv(arr):
     return
 
 
-# Create and configure logger
-logging.basicConfig(filename="newfile.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
+def init_logger():
+    logging.basicConfig(filename='newfile.log', format='%(asctime)s %(message)s', filemode='w')
+    return logging.getLogger()
 
-# Creating an object
-logger = logging.getLogger()
 
-# check inout type and run user_or_repo or all_repo
-# create parser object
-parser = argparse.ArgumentParser(description="A text file manager!")
+def init_parser():
+    parser = argparse.ArgumentParser(description='Parses cmd line args for git-compare')
+    parser.add_argument("-u", "--user", type=str, nargs='*',
+                        metavar="user_name", default=None, dest='user_names',
+                        help="Opens and reads the specified text file.")
 
-# defining arguments for parser object
-parser.add_argument("-u", "--user", type=str, nargs='*',
-                    metavar="user_name", default=None, dest='user_names',
-                    help="Opens and reads the specified text file.")
+    parser.add_argument("-r", "--repo", type=str, nargs='*',
+                        metavar="repo", default=None, dest='repo_names',
+                        help="Shows all the text files on specified directory path.\
+                        Type '.' for current directory.")
 
-parser.add_argument("-r", "--repo", type=str, nargs='*',
-                    metavar="repo", default=None, dest='repo_names',
-                    help="Shows all the text files on specified directory path.\
-                    Type '.' for current directory.")
+    parser.add_argument("-o", "--outputtype", type=str, nargs=1,
+                        metavar="output_dest", default=['cmd'], dest='out_type',
+                        help="Check whether the output should be printed in cmd, csv, html or json.\
+                        Type '.' for current directory.")
+    return parser
 
-parser.add_argument("-o", "--outputtype", type=str, nargs=1,
-                    metavar="output_dest", default=['cmd'], dest='out_type',
-                    help="Check whether the output should be printed in cmd, csv, html or json.\
-                    Type '.' for current directory.")
 
-# parse the arguments from standard input
-args = parser.parse_args()
+def main():
+    logger = init_logger()
+    arg_parser = init_parser()
+    args = arg_parser.parse_args()
+    # calling functions depending on type of argument
+    if args.user_names is not None:
+        # print('user analysis')
+        logger.info("user analysis")
+        data = user_or_repo(args.user_names)
+        details = filter_data(data)
+    elif args.repo_names is not None:
+        # print('repo analysis')
+        logger.info("repo analysis")
+        data = user_or_repo(args.repo_names, 'repo')
+        details = filter_data(data, 'repo')
+    else:
+        # print('no specifications')
+        logger.info("no specifications")
+        data = user_or_repo(args.user_names)  # just for now
+        details = filter_data(data)
 
-# calling functions depending on type of argument
-if args.user_names is not None:
-    # print('user analysis')
-    logger.info("user analysis")
-    data = user_or_repo(args.user_names)
-    details = filter_data(data)
-elif args.repo_names is not None:
-    # print('repo analysis')
-    logger.info("repo analysis")
-    data = user_or_repo(args.repo_names, 'repo')
-    details = filter_data(data, 'repo')
-else:
-    # print('no specifications')
-    logger.info("no specifications")
-    data = user_or_repo(args.user_names)  # just for now
-    details = filter_data(data)
+    res = []
 
-res = []
+    for i in details.keys():
+        res.append([len(res) + 1, i] + details[i])
 
-for i in details.keys():
-    res.append([len(res) + 1, i] + details[i])
+    for i in range(len(res)):
+        for j in range(len(res[0])):
+            res[i][j] = str(res[i][j])
 
-for i in range(len(res)):
-    for j in range(len(res[0])):
-        res[i][j] = str(res[i][j])
+    # check output type
+    # return table, html, json or csv
+    x = PrettyTable()
+    if 'login' in details.keys():
+        x.field_names = ["S.No.", "Arg"] + details['login']
+    else:
+        x.field_names = ["S.No.", "Arg"] + details['full_name']
+    x.add_rows(res)
+    x.sortby = None
+    x.hrules = ALL
 
-# check output type
-# return table, html, json or csv
-x = PrettyTable()
-if 'login' in details.keys():
-    x.field_names = ["S.No.", "Arg"] + details['login']
-else:
-    x.field_names = ["S.No.", "Arg"] + details['full_name']
-x.add_rows(res)
-x.sortby = None
-x.hrules = ALL
+    if args.out_type[0] == 'cmd':
+        # print('output to cmd')
+        logger.info("output to cmd")
+        print(x)
+    elif args.out_type[0] == 'csv':
+        # print('output to csv')
+        logger.info("output to csv")
+        create_csv(res)
+    elif args.out_type[0] == 'html':
+        # print('output to html')
+        logger.info("output to html")
+        print(x.get_html_string())
+    elif args.out_type[0] == 'json':
+        # print('output to json')
+        logger.info("output to json")
+        print(x.get_json_string())
 
-if args.out_type[0] == 'cmd':
-    # print('output to cmd')
-    logger.info("output to cmd")
-    print(x)
-elif args.out_type[0] == 'csv':
-    # print('output to csv')
-    logger.info("output to csv")
-    create_csv(res)
-elif args.out_type[0] == 'html':
-    # print('output to html')
-    logger.info("output to html")
-    print(x.get_html_string())
-elif args.out_type[0] == 'json':
-    # print('output to json')
-    logger.info("output to json")
-    print(x.get_json_string())
+
+if __name__ == '__main__':
+    main()
