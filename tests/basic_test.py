f1611b614f60bc25978c6141dd08b25d53a12f10
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-07-15 22:32:23 +0530

    reformat to follow pep 8

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -7,21 +7,24 @@ Baisc test cases dependent on GitComp class output (as caputured in capsys)
 - null args
 """
 
+
 def test_user(capsys):
     gitcomp.GitComp(['avaish1409'])
-    
+
     out, err = capsys.readouterr()
     print(out)
     assert err == ''
 
+
 def test_repo(capsys):
     gitcomp.GitComp(repos=['avaish1409/VideoChatBot'])
-    
+
     out, err = capsys.readouterr()
     assert err == ''
 
+
 def test_null(capsys):
     gitcomp.GitComp()
-    
+
     out, err = capsys.readouterr()
     assert err == ''
